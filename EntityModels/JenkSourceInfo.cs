ef8af043b56ad240fac349d623c8d7229fe0dca7
rathinam Shanmugam <rathinam@zilliontechnologies.com>
2021-07-14 15:24:45 +0530

    updated

--- EntityModels/JenkSourceInfo.cs
+++ EntityModels/JenkSourceInfo.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
 // #nullable disable
-
+//Addded
 namespace ConsoleApp1.EntityModels
 {
     public partial class JenkSourceInfo
